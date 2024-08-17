0644c1669ff348e08b12f84303b252620601d0ab
Andrii D. <and05gt@gmail.com>
2024-08-17 22:42:41 +0200

    fix about section

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -4,24 +4,45 @@ import 'swiper/css';
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-// const swiper = new Swiper('.swiper', {
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//   },
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: true,
-//     pageUpDown: true,
-//   },
-//   mousewheel: {
-//     invert: true,
-//   },
-//   touchEventsTarget: 'wrapper',
-//   slideClass: 'about-skills-item',
-//   slideActiveClass: 'about-skills-item-accent',
-//   wrapperClass: 'about-skills-list',
-// });
+const swiperContainer = document.querySelector('.swiper');
 
+const swiper = new Swiper(swiperContainer, {
+  initialSlide: 0,
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  mousewheel: {
+    invert: true,
+  },
+  touchEventsTarget: 'wrapper',
+  slideClass: 'about-skills-item',
+  slideActiveClass: 'about-skills-item-accent',
+  wrapperClass: 'about-skills-list',
+  rewind: true,
+
+  breakpoints: {
+    // when window width is >= 375px
+    375: {
+      slidesPerView: 2,
+      width: 260,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      width: 600,
+    },
+    // when window width is >= 1440px
+    1440: {
+      slidesPerView: 6,
+      width: 1200,
+    },
+  },
+});
 const container = document.querySelector('.about-accordion-container');
 
 const accordion = new Accordion(container, {
