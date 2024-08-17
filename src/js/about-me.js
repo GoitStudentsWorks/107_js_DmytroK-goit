c332df0503e17131c4a19ad6e7af3c7fadfff829
Andrii D. <and05gt@gmail.com>
2024-08-17 13:43:57 +0200

    add accordion styles, icons and js

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,20 +1,33 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 
-const swiper = new Swiper('.swiper', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    pageUpDown: true,
-  },
-  mousewheel: {
-    invert: true,
-  },
-  touchEventsTarget: 'wrapper',
-  slideClass: 'about-skills-item',
-  slideActiveClass: 'about-skills-item-accent',
-  wrapperClass: 'about-skills-list',
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+// const swiper = new Swiper('.swiper', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//   },
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: true,
+//     pageUpDown: true,
+//   },
+//   mousewheel: {
+//     invert: true,
+//   },
+//   touchEventsTarget: 'wrapper',
+//   slideClass: 'about-skills-item',
+//   slideActiveClass: 'about-skills-item-accent',
+//   wrapperClass: 'about-skills-list',
+// });
+
+const container = document.querySelector('.about-accordion-container');
+
+const accordion = new Accordion(container, {
+  openOnInit: [0],
+  showMultiple: true,
+  elementClass: 'about-ac',
+  panelClass: 'about-ac-panel',
+  triggerClass: 'about-ac-btn',
 });
