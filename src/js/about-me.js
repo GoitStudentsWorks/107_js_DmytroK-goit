40bbe2a4d8c3d9cf9c7e5b1a5e3bd393171797b1
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-08-22 00:11:04 +0300

    a

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -43,3 +43,13 @@ const swiper = new Swiper(swiperCont, {
     },
   },
 });
+const container = document.querySelector('.about-accordion-container');
+
+const accordion = new Accordion(container, {
+  openOnInit: [0],
+  showMultiple: true,
+  duration: 500,
+  elementClass: 'about-ac',
+  panelClass: 'about-ac-panel',
+  triggerClass: 'about-ac-btn',
+});
